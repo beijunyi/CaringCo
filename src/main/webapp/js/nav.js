eedb08ab52e27bd6416758d778559d637dd68bb4
beijunyi <junyi.bei@formicary.net>
2014-02-28 22:30:59 +0000

    key press to switch views

--- src/main/webapp/js/nav.js
+++ src/main/webapp/js/nav.js
@@ -1,7 +1,38 @@
-app.controller('NavigationController', ['$scope', '$location',
-  function($scope, $location) {
-    $scope.isActive = function(route) {
-      return route === $location.path();
+app.controller('NavigationController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
+  $(document).keydown(function (e) {
+
+    var home = "/home", about = "/about", contact = "/contact";
+
+    function view(path) {
+      $rootScope.$apply(function() {
+        $location.path(path);
+      });
     }
+
+    var key = e.keyCode;
+    if(key == 37 || key == 39) {
+      var path = $location.path();
+      if(key == 37) {
+        if(path == home)
+          view(contact);
+        else if(path == contact)
+          view(about);
+        else if(path == about)
+          view(home);
+      } else {
+        if(path == home)
+          view(about);
+        else if(path == about)
+          view(contact);
+        else if(path == contact)
+          view(home);
+      }
+      return false;
+    }
+    return true;
+  });
+  $scope.isActive = function (route) {
+    return route === $location.path();
   }
+}
 ]);
